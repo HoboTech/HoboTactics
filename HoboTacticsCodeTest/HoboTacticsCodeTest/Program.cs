03fa224f30a1c617000cfa41fd67609a94d49271
Daniel Cecil <cecil15620@gmail.com>
2013-09-19 22:23:14 -0700

    Initial C# Test Project.
    Hello World.

---
+++ HoboTacticsCodeTest/HoboTacticsCodeTest/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HoboTacticsCodeTest
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
