6bb460f717a22f1244e43ebe7ba2aa026421a606
shagAttack <dglatimer@gmail.com>
2013-09-29 19:34:24 -0700

    Advanced tutorial to part 4. Added line clase (line valuse currently hardcoded).

--- Win8ShooterGame/Win8ShooterGame/Game1.cs
+++ Win8ShooterGame/Win8ShooterGame/Game1.cs
@@ -10,6 +10,8 @@ namespace Win8ShooterGame
     {
         GraphicsDeviceManager _graphics;
         SpriteBatch _spriteBatch;
+        Player player;
+        Line line;
 
         public Game1()
         {
@@ -25,10 +27,14 @@ namespace Win8ShooterGame
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            // Initialize the player class
+            player = new Player();
+            line = new Line();
 
             base.Initialize();
         }
+
+
 
         /// <summary>
         /// LoadContent will be called once per game and is the place to load
@@ -39,7 +45,12 @@ namespace Win8ShooterGame
             // Create a new SpriteBatch, which can be used to draw textures.
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            // Load the player resources
+
+            Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+            player.Initialize(Content.Load<Texture2D>("Graphics\\player"), playerPosition);
+            line.Initialize(Content.Load<Texture2D>("Graphics\\white_1x1"));
+
         }
 
         /// <summary>
@@ -69,9 +80,15 @@ namespace Win8ShooterGame
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+            // Start drawing
+            _spriteBatch.Begin();
+            // Draw the Player
+            player.Draw(_spriteBatch);
+            line.Draw(_spriteBatch);
+            // Stop drawing
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ Win8ShooterGame/Win8ShooterGame/Line.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Win8ShooterGame
+{
+    class Line
+    {
+        public Texture2D pixelTexture;
+        Vector2 begin = new Vector2(20, 20);
+        Vector2 end = new Vector2(110, 110);
+        Color color = Color.White;
+
+        public void Initialize(Texture2D texture)
+        {
+            pixelTexture = texture;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 begin, Vector2 end, Color color, int width = 1)
+        {
+            Rectangle r = new Rectangle((int)begin.X, (int)begin.Y, (int)(end - begin).Length() + width, width);
+            Vector2 v = Vector2.Normalize(begin - end);
+            float angle = (float)Math.Acos(Vector2.Dot(v, -Vector2.UnitX));
+            if (begin.Y > end.Y) angle = MathHelper.TwoPi - angle;
+            spriteBatch.Draw(pixelTexture, r, null, color, angle, Vector2.Zero, SpriteEffects.None, 0);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            int width = 1;
+            Rectangle r = new Rectangle((int)begin.X, (int)begin.Y, (int)(end - begin).Length() + width, width);
+            Vector2 v = Vector2.Normalize(begin - end);
+            float angle = (float)Math.Acos(Vector2.Dot(v, -Vector2.UnitX));
+            if (begin.Y > end.Y) angle = MathHelper.TwoPi - angle;
+            spriteBatch.Draw(pixelTexture, r, null, color, angle, Vector2.Zero, SpriteEffects.None, 0);
+        }
+
+    }
+}
